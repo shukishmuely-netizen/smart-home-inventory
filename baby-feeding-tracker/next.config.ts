8fe1d1130aea73f02a5864d5f65cdc63a1e5efa5
Shuki Shmuely <shukishmuely@mac.home>
2026-03-30 13:24:01 +0300

    Add WhatsApp share deep-link upon saving a new task

--- baby-feeding-tracker/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  // ביטול מפורש של Turbopack במידה והוא מנסה להיכנס כברירת מחדל
-  transpilePackages: [], 
-};
-
-export default nextConfig;
